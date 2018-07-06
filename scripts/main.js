f9f4b0c05539b1284aeb277b40b56d560be88f5d
Drew <drewfisher1234@gmail.com>
2018-07-06 10:00:07 -0400

    Lots o changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
-document.querySelector('h1').onclick = function() {
-    var Title = document.querySelector('h1');
-    currentText = document.getElementById("Title").textContent;
-    if (currentText == "Andrew Fisher"){
-        Title.textContent= "Portfolio"
-    }else{
-        Title.textContent= "Andrew Fisher"
-    }
-}
+// document.querySelector('h1').onclick = function() {
+//     var name = document.querySelector('h1');
+//     currentText = document.getElementById("name").textContent;
+//     if (currentText == "Andrew Fisher"){
+//         Title.textContent= "Portfolio"
+//     }else{
+//         Title.textContent= "Andrew Fisher"
+//     }
+// }
