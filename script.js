784471cf8cb85382926b639b44caede8897672d7
Drew <drewfisher1234@gmail.com>
2018-07-17 19:57:31 -0400

    Another revision

--- script.js
+++ script.js
@@ -12,66 +12,66 @@ let board = [
 
 // const location = document.getElementById("connectfour");
 
-function checkWin(cellRow,cellColumn){
+function checkWin(cellRow, cellColumn) {
 
     //Checking for horizontal wins on row which triggered event
-    for(let item in board[cellRow]){
+    for (let item in board[cellRow]) {
         item = Number(item);
         let cell = board[cellRow][item];
-        if(cell != 0 && cell === board[cellRow][item+1] && cell === board[cellRow][item+2] && cell === board[cellRow][item+3]){
+        if (cell != 0 && cell === board[cellRow][item + 1] && cell === board[cellRow][item + 2] && cell === board[cellRow][item + 3]) {
             return true;
         }
     }
 
     //Checking for vertical wins on the column which triggered event
-    for(let j = 0; j <= 2; j++){
+    for (let j = 0; j <= 2; j++) {
         item = Number(j);
         let cell = board[item][cellColumn];
-        if(cell != 0 && cell === board[item+1][cellColumn] && cell === board[item+2][cellColumn] && cell === board[item+3][cellColumn]){
+        if (cell != 0 && cell === board[item + 1][cellColumn] && cell === board[item + 2][cellColumn] && cell === board[item + 3][cellColumn]) {
             return true;
         }
     }
 
     //Checking for downwards right diagonal wins across the whole board because IDK how to only check part of the board
-    for(let i = 0; i <= 2; i++){
-        for(let j = 0; j <=3; j++){
+    for (let i = 0; i <= 2; i++) {
+        for (let j = 0; j <= 3; j++) {
             let cell = board[i][j];
-            if(cell != 0 && cell === board[i+1][j+1] && cell === board[i+2][j+2] && cell === board[i+3][j+3]){
+            if (cell != 0 && cell === board[i + 1][j + 1] && cell === board[i + 2][j + 2] && cell === board[i + 3][j + 3]) {
                 return true;
             }
         }
     }
 
     //Checking for downwards left diagonal wins across the whole board because IDK how to only check part of the board
-    for(let i = 0; i <= 2; i++){
-        for(let j = 3; j<= 6; j++){
+    for (let i = 0; i <= 2; i++) {
+        for (let j = 3; j <= 6; j++) {
             let cell = board[i][j];
-            if(cell != 0 && cell === board[i+1][j-1] && cell === board[i+2][j-2] && cell === board[i+3][j-3]){
+            if (cell != 0 && cell === board[i + 1][j - 1] && cell === board[i + 2][j - 2] && cell === board[i + 3][j - 3]) {
                 return true;
             }
         }
     }
 }
 
-function checkTie(){
+function checkTie() {
     let sum = 0;
-    for(let item in board[0]){
-        if(board[0][item] === 0){
+    for (let item in board[0]) {
+        if (board[0][item] === 0) {
             sum = 0;
             return false;
-        }else{
+        } else {
             sum++
         }
     }
-    if(sum >= 7){
+    if (sum >= 7) {
         return true;
     }
 }
 
-function getWinningPlayer(){
-    if(redTurn){
+function getWinningPlayer() {
+    if (redTurn) {
         return "Red";
-    }else{
+    } else {
         return "Black";
     }
 }
@@ -101,9 +101,9 @@ handleClick = function (event) {
     }
 
     //Checking if someone won or if there is a tie. If so the game stops and resets. If not the player switches.
-    if(checkWin(cellRow,cellColumn)){
+    if (checkWin(cellRow, cellColumn)) {
         let winningPlayer = getWinningPlayer();
-        alert("Player "+winningPlayer+" has won!!!");
+        alert("Player " + winningPlayer + " has won!!!");
         board = [
             [0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0],
@@ -119,8 +119,8 @@ handleClick = function (event) {
             cells[i].className = ('blank');
         }
 
-        
-    }else if (checkTie()){
+
+    } else if (checkTie()) {
         alert("This is a tie. Congratulations on both being losers.");
         board = [
             [0, 0, 0, 0, 0, 0, 0],
@@ -137,9 +137,9 @@ handleClick = function (event) {
             cells[i].className = ('blank');
         }
 
-    }else{
+    } else {
         redTurn = !redTurn;
-        }
+    }
 }
 
 //Adding Event Listeners
@@ -148,4 +148,74 @@ for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener('click', handleClick);
 }
 
-// Images
+// Rock Paper Scissors
+const wonTag = document.getElementById("whoWon");
+const computerImg = document.getElementById("computerImg");
+let userScore = 0;
+let compScore = 0;
+const rockButton = document.getElementById("rockButton");
+const paperButton = document.getElementById("paperButton");
+const scissorsButton = document.getElementById("scissorsButton");
+rockButton.onclick = function () {
+    runGame(0);
+}
+paperButton.onclick = function () {
+    runGame(1);
+}
+scissorsButton.onclick = function () {
+    runGame(2);
+}
+
+function runGame(userPlay) {
+    let computerPlay = getRandomInt(0, 3);
+    computerImageSwitch(computerPlay);
+    let winner = checkWinner(userPlay, computerPlay);
+    if (winner == "user") {
+        userScore++;
+        userScore_label.textContent = userScore;
+        //alert("The winner is the User!");
+        wonTag.textContent = ("The User won this round!")
+    } else if (winner == "comp") {
+        compScore++;
+        compScore_label.textContent = compScore;
+        //alert("The winner is the Computer :,(");
+        wonTag.textContent = ("The Computer won this round :,(")
+    } else if (winner == "tie") {
+        //alert("The game is a tie!")
+        wonTag.textContent = ("This round is a tie.")
+    }
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function checkWinner(user, comp) {
+    if (user == comp) {
+        return ("tie");
+    } else if (user == 0 && comp == 2) {
+        return ("user");
+    } else if (user == 1 && comp == 0) {
+        return ("user");
+    } else if (user == 2 && comp == 1) {
+        return ("user");
+    } else if (comp == 0 && user == 2) {
+        return ("comp");
+    } else if (comp == 1 && user == 0) {
+        return ("comp");
+    } else if (comp == 2 && user == 1) {
+        return ("comp")
+    }
+}
+
+function computerImageSwitch(pick) {
+    if (pick == 0) {
+        computerImg.setAttribute("src", "rock.png")
+    } else if (pick == 1) {
+        computerImg.setAttribute("src", "paper.png")
+    } else if (pick == 2) {
+        computerImg.setAttribute("src", "scissors.png")
+    }
+}
